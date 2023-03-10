bb6ad75f5a1dee425ec5f83040336cae69c117cc
于殿龙 <jellydicky@outlook.com>
2023-03-10 08:38:45 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="这是一个cli模板" />
   </div>
 </template>
 
